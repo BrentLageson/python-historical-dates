5d13bdf9c02b5c5f8089bed8b899a864f37c40ec
BrentLageson <Brent.Lageson@gmail.com>
2024-04-28 15:07:51 -0500

    Additional linting fixes

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ This module provides setup info for python
 """
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('README.md', mode='r', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name='historicaldates',
     version='1.0.0',
     author='Brent Lageson',
     author_email='brent.lageson@gmail.com',
     description='Python library for handling BCE and CE dates.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/BrentLageson/python-historical-dates',
     packages=find_packages(),
